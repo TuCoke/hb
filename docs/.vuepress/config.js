75f9116aea6c8027f7f7112587c6e4af22188a22
Bo-Hong <2764136779@qq.com>
2025-05-25 20:10:50 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
   lang: 'en-US',
-
+  base: "/hb/",
   title: 'VuePress',
   description: 'My first VuePress Site',
 
