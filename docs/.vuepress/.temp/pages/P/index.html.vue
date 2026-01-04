388ec78f8a2889c9edc9d54eebd10840a786bc1f
Bo-Hong <2764136779@qq.com>
2026-01-04 20:42:27 +0800

    优化.gitignore文件，排除VuePress临时文件和编译产物
    - 添加VuePress临时文件和缓存目录排除规则
    - 完善Node.js、IDE、操作系统文件排除规则
    - 移除已跟踪的VuePress临时文件

--- docs/.vuepress/.temp/pages/P/index.html.vue
+++
@@ -1,6 +0,0 @@
-<template><div><h1 id="个人总结" tabindex="-1"><a class="header-anchor" href="#个人总结"><span>个人总结</span></a></h1>
-<h2 id="解决问题能力" tabindex="-1"><a class="header-anchor" href="#解决问题能力"><span>解决问题能力</span></a></h2>
-<h2 id="" tabindex="-1"><a class="header-anchor" href="#"><span></span></a></h2>
-</div></template>
-
-
